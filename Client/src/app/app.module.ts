0e8225898af77d008a71f1f88407268de10847e2
Khayyam Adeel <72245755+Khayyam-Adeel@users.noreply.github.com>
2024-04-27 15:33:54 +0500

    add mkcert for ssl  expries on 27 july 2026

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ErrorsComponent } from './errors/errors.component';
 import { ErrorInterceptor } from './_interceptor/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
-import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberCardComponent } from './Members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
